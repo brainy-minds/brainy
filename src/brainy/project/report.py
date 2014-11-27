7be673c3e60868e4f6dd8764f64f8170369e7865
Yauhen Yakimovich <eugeny.yakimovitch@gmail.com>
2014-11-27 15:28:33 +0100

    modified:   src/brainy/project/report.py - reports are duplicated in JSON. YAML is a human readable.. later should be   generated by template. - started/finished entries are now strings, not timestamps.

--- src/brainy/project/report.py
+++ src/brainy/project/report.py
@@ -1,4 +1,5 @@
 import os
+import json
 import yaml
 from datetime import datetime
 from brainy.log import json_handler
@@ -15,21 +16,27 @@ def get_now_str():
 
 
 def start_report():
-    report_data['started_at'] = datetime.now()
+    report_data['started_at'] = get_now_str()
 
 
 def finalize_report():
     '''Produce final report structure as JSON'''
-    report_data['finished_at'] = datetime.now()
+    report_data['finished_at'] = get_now_str()
     report_data['log'] = json_handler.houtput.root  # points to dictionary
 
 
-def save_report(report_filepath):
+def save_report(report_filepath, produce_json=True):
     reports_folder = os.path.dirname(report_filepath)
     if not os.path.exists(reports_folder):
         logger.info('Creating missing reports folder: %s' % reports_folder)
         os.makedirs(reports_folder)
     now_str = datetime.now().strftime('%Y_%m_%d_%H%M%S')
-    report_filepath = '%s-report-%s.yaml' % (report_filepath, now_str)
-    with open(report_filepath, 'w+') as reportfile:
-        reportfile.write(yaml.dump(report_data, default_flow_style=False))
+    # Output a human readable YAML file.
+    yaml_report_filepath = '%s-report-%s.yaml' % (report_filepath, now_str)
+    with open(yaml_report_filepath, 'w+') as yaml_reportfile:
+        yaml_reportfile.write(yaml.dump(report_data, default_flow_style=False))
+    # Optionally duplicate it as a json.
+    if produce_json:
+        json_report_filepath = '%s-report-%s.json' % (report_filepath, now_str)
+        with open(json_report_filepath, 'w+') as json_reportfile:
+            json_reportfile.write(json.dumps(report_data))
